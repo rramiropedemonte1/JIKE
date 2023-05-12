a53376e9cb3285105658cb09cc63e571eb851477
Ramiro <ramiivan59@gmail.com>
2023-05-12 13:53:43 -0300

    primer commit

---
+++ js/array.js
@@ -0,0 +1,481 @@
+const sneakers = [
+  {
+    //AIR JORDAN 1//
+
+    imagen: "./img/jordan 1/jordan 1 low x Travis Scott.jpg",
+    id: 1,
+    nombre: "Air Jordan 1 low x Travis Scott",
+    precio: "80.000",
+  },
+  {
+    imagen: "./img/jordan 1/jordan low SE paris.jpeg",
+    id: 2,
+    nombre: "Air Jordan 1 low SE Paris",
+    precio: "50.000",
+  },
+  {
+    imagen: "./img/jordan 1/jordan 1 mid Elephant toe.webp",
+    id: 3,
+    nombre: "Air Jordan 1 mid Elephant",
+    precio: "61.000",
+  },
+  {
+    imagen: "../img/jordan 1/travis.jpeg",
+    id: 4,
+    nombre: "Air Jordan 1 High x Travis Scott",
+    precio: "70.000",
+  },
+  {
+    imagen: "../img/jordan 1/jordan 1 retro low travis black panthom.webp",
+    id: 5,
+    nombre: "Air Jordan 1  Panthom Travis Scott",
+    precio: "245.000",
+  },
+  {
+    imagen: "../img/jordan 1/Air Jordan 1 High OG 'Rebellionaire'.webp",
+    id: 6,
+    nombre: "Air Jordan 1 High Rebellionair",
+    precio: "65.000",
+  },
+  {
+    imagen: "../img/jordan 1/Air Jordan 1 low UNC.jpeg",
+    id: 7,
+    nombre: "Air Jordan 1 low UNC",
+    precio: "56.000",
+  },
+  {
+    imagen: "../img/jordan 1/dior x air jordan 1 high.jpeg",
+    id: 8,
+    nombre: "Air Jordan 1 high x Dior",
+    precio: "241.000",
+  },
+  {
+    imagen: "../img/jordan 1/jordan 1 high travis.webp",
+    id: 9,
+    nombre: "Air Jordan high Dark Mocha",
+    precio: "60.000",
+  },
+  {
+    imagen: "../img/jordan 1/jordan 1 low light smoke grey.jpeg",
+    id: 10,
+    nombre: "Air Jordan 1 low Light Smoke Grey",
+    precio: "52.200",
+  },
+  {
+    imagen: "../img/jordan 1/jordan 1 low washed denim.webp",
+    id: 11,
+    nombre: "Air Jordan 1 low Washed Denim",
+    precio: "54.500",
+  },
+  {
+    imagen: "../img/jordan 1/jordan 1 mid gs Schematic.jpg",
+    id: 12,
+    nombre: "Air Jordan 1 mid Schematic",
+    precio: "60.000",
+  },
+  {
+    imagen: "../img/jordan 1/jordan 1 mid racer blue.jpeg",
+    id: 13,
+    nombre: "Air Jordan 1 mid Racer Blue",
+    precio: "58.700",
+  },
+  {
+    imagen: "../img/jordan 1/jordan 1 mid.jpg",
+    id: 14,
+    nombre: "Air Jordan 1 mid",
+    precio: "52.000",
+  },
+  {
+    imagen:
+      "../img/jordan 1/Jordan 1 Retro High White University Blue Black.jpeg",
+    id: 15,
+    nombre: "Air Jordan 1 high white University Blue Black",
+    precio: "76.000",
+  },
+
+  //AIR JORDAN 3//
+
+  {
+    imagen: "./img/jordan 3/Air Jordan 3 Retro 'Triple White'.jpeg",
+    id: 16,
+    nombre: "Air Jordan 3 Retro Triple White",
+    precio: "60.000",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 Retro 'UNC'.jpeg",
+    id: 17,
+    nombre: "Air Jordan 3 Retro UNC",
+    precio: "62.400",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 High Travis Scott Antique Brass.jpeg",
+    id: 18,
+    nombre: "Air Jordan 3 high Travis Scott",
+    precio: "145.000",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 Retro ''Cardinal Red''.jpeg",
+    id: 19,
+    nombre: "Air Jordan Retro 3 Cardinal Red",
+    precio: "102.000",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 Retro 'Barely Grape'.jpeg",
+    id: 20,
+    nombre: "Air Jordan 3 Retro Barely Grape",
+    precio: "105.000",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 Retro Black Cat.webp",
+    id: 21,
+    nombre: "Air Jordan Retro 3 Black Cat",
+    precio: "156.000",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 Retro 'Fragment'.jpg",
+    id: 22,
+    nombre: "Air Jordan Retro 3 Fragment",
+    precio: "98.500",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 Retro 'Hall of Fame'.jpg",
+    id: 23,
+    nombre: "Air Jordan Retro 3 Hall of Fame",
+    precio: "121.000",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 Retro 'Knicks'.jpeg",
+    id: 24,
+    nombre: "Air Jordan Retro 3 Knicks",
+    precio: "123.000",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 Retro Lucky Green.webp",
+    id: 25,
+    nombre: "Air Jordan Retro 3 Lucky Green",
+    precio: "100.000",
+  },
+  {
+    imagen: "../img/jordan 3/Nike AirJordan 3 Retro Tinker Hatfield.jpeg",
+    id: 26,
+    nombre: "Air Jordan Retro 3 Tinker Hatfield",
+    precio: "98.000",
+  },
+  {
+    imagen: "../img/jordan 3/Air Jordan 3 Retro Tinker.webp",
+    id: 27,
+    nombre: "Air Jordan Retro 3 Tinker",
+    precio: "96.700",
+  },
+
+  //Air FORCE//
+
+  {
+    imagen: "../img/air force/Air Force 1 Low '07 SE 'Pearl White'.jpg",
+    id: 28,
+    nombre: "Air Force 1 low 07 SE Pearl White",
+    precio: "42.500",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 '07 Low 'White'.jpg",
+    id: 29,
+    nombre: "Air Force 1 low",
+    precio: "40.000",
+  },
+  {
+    imagen:
+      "../img/air force/Nike Air Force 1 Low 'Spray Paint Swoosh White Black Grey'.webp",
+    id: 30,
+    nombre: "Air Force 1 low Spray Paint",
+    precio: "41.700",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 '07 Low 'New York'.jpg",
+    id: 31,
+    nombre: "Air Force 1 low 07 NY",
+    precio: "58.700",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 '07 Low.jpg",
+    id: 32,
+    nombre: "Air Force low Paint",
+    precio: "56.600",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 '07 LE 'Starfish'.webp",
+    id: 33,
+    nombre: "Air Force 1 low 07 Starfish",
+    precio: "54.500",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 '07 Low 'NBA'.webp",
+    id: 34,
+    nombre: "Air Force 1 low NBA",
+    precio: "120.400",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 '07 Low 'Polaroid'.webp",
+    id: 35,
+    nombre: "Air Force 1 low Polaroid",
+    precio: "53.000",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 High Utility 'Force is Female'.webp",
+    id: 36,
+    nombre: "Air Force 1 high Female",
+    precio: "67.700",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 Low QS 'Skeleton'.webp",
+    id: 37,
+    nombre: "Air Force 1 low Skeleton",
+    precio: "58.900",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 Mid 'Broken silk'.webp",
+    id: 38,
+    nombre: "Air Force 1 mid Broken Silk",
+    precio: "78.000",
+  },
+  {
+    imagen: "../img/air force/Nike Air Force 1 Low 'Stussy'.webp",
+    id: 39,
+    nombre: "Air Force 1 low Stussy",
+    precio: "65.000",
+  },
+  {
+    imagen: "../img/air force/Air Force 1 Mid '07 LV8 'White Black'.webp",
+    id: 40,
+    nombre: "Air Force 1 mid 07 LV8 White Black",
+    precio: "68.000",
+  },
+
+  //DUNK//
+
+  {
+    imagen: "./img/nike dunk/dunk light bone.webp",
+    id: 41,
+    nombre: "Nike Dunk low Light Bone",
+    precio: "51.500",
+  },
+  {
+    imagen: "./img/nike dunk/Dunk Low 'Green'.jpg",
+    id: 42,
+    nombre: "Nike Dunk low Green",
+    precio: "55.000",
+  },
+  {
+    imagen: "../img/nike dunk/Dunk Low OG 'Purple Pulse'.jpg",
+    id: 43,
+    nombre: "Nike Dunk OG Purple Pulse",
+    precio: "51.200",
+  },
+  {
+    imagen: "../img/nike dunk/Dunk Low Disrupt 2 'Pale Ivory'.jpg",
+    id: 44,
+    nombre: "Nike Dunk Low Disprut 2 Pale Ivory",
+    precio: "53.600",
+  },
+  {
+    imagen: "../img/nike dunk/Dunk High 'Loading'.webp",
+    id: 45,
+    nombre: "Nike Dunk high Loading",
+    precio: "65.000",
+  },
+  {
+    imagen: "../img/nike dunk/Dunk Low GS 'Just Stitch It - Hyper Royal'.webp",
+    id: 46,
+    nombre: "Nike Dunk low GS Hyper Royal",
+    precio: "54.500",
+  },
+  {
+    imagen: "../img/nike dunk/Dunk Low Pro SB 'Classic Green'.webp",
+    id: 47,
+    nombre: "Nike Dunk low SB Classic Green",
+    precio: "50.000",
+  },
+  {
+    imagen:
+      "../img/nike dunk/Nike StrangeLove x Dunk Low SB 'Valentine's Day'.jpeg",
+    id: 48,
+    nombre: "Nike Dunk low SB Valentine's Day",
+    precio: "65.200",
+  },
+  {
+    imagen:
+      "../img/nike dunk/Supreme x Dunk High SB 'By Any Means - Brazil'.webp",
+    id: 49,
+    nombre: "Nike Dunk Supreme high Brazil",
+    precio: "87.900",
+  },
+  {
+    imagen:
+      "../img/nike dunk/Supreme x Dunk High SB 'By Any Means - Stormtrooper'.webp",
+    id: 50,
+    nombre: "Nike Dunk high SB x Supreme Stormtrooper",
+    precio: "97.000",
+  },
+  {
+    imagen: "../img/nike dunk/Supreme x Nike SB Dunk High.jpeg",
+    id: 51,
+    nombre: "Nike Dunk high SB x Supreme",
+    precio: "69.900",
+  },
+  {
+    imagen:
+      "../img/nike dunk/Nike StrangeLove x Dunk Low SB 'Valentine's Day rosa'.webp",
+    id: 52,
+    nombre: "Nike Dunk low SB Valentine's Day",
+    precio: "65.200",
+  },
+
+  //AIR JORDAN 4//
+
+  {
+    imagen: "./img/jordan 4/Air Jordan 4 Retro 'University Blue'.jpeg",
+    id: 53,
+    nombre: "Air Jordan Retro 4 University Blue",
+    precio: "72.500",
+  },
+  {
+    imagen: "./img/jordan 4/Air Jordan 4 Retro 'Travis Scott Cactus Jack'.jpeg",
+    id: 54,
+    nombre: "Air Jordan Retro 4 Travis Scott",
+    precio: "120.000",
+  },
+  {
+    imagen: "../img/jordan 4/Nike AirJordan 4 Retro Off-White Sail.jpg",
+    id: 55,
+    nombre: "Air Jordan 4 Retro Off White",
+    precio: "240.000",
+  },
+  {
+    imagen: "../img/jordan 4/Air Jordan 4 Retro 'Black Cat' 2020.jpeg",
+    id: 56,
+    nombre: "Air Jordan Retro 4 Black Cat",
+    precio: "200.000",
+  },
+  {
+    imagen: "../img/jordan 4/Air Jordan 4 Retro 'Canyon Purple'.webp",
+    id: 57,
+    nombre: "Air Jordan Retro 4 Canyon Purple",
+    precio: "154.000",
+  },
+  {
+    imagen: "../img/jordan 4/Air Jordan 4 Retro 'Infrared'.jpg",
+    id: 58,
+    nombre: "Air Jordan Retro 4 Infrared",
+    precio: "145.000",
+  },
+  {
+    imagen: "../img/jordan 4/Air Jordan 4 Retro 'Levi's Black'.jpeg",
+    id: 59,
+    nombre: "Air Jordan Retro 4 Levi's Black",
+    precio: "121.000",
+  },
+  {
+    imagen: "../img/jordan 4/Air Jordan 4 Retro 'Military Black'.webp",
+    id: 60,
+    nombre: "Air Jordan Retro 4 Military Black",
+    precio: "120.000",
+  },
+  {
+    imagen: "../img/jordan 4/Air Jordan 4 Retro 'Motorsports'.jpeg",
+    id: 61,
+    nombre: "Air Jordan Retro 4 Motorsports",
+    precio: "145.000",
+  },
+  {
+    imagen: "../img/jordan 4/Air Jordan 4 Retro 'Thunder'.webp",
+    id: 62,
+    nombre: "Air Jordan Retro 4 Thunder",
+    precio: "133.000",
+  },
+  {
+    imagen: "../img/jordan 4/Air Jordan 4 Retro OG 'White Cement' 2016.jpeg",
+    id: 63,
+    nombre: "Air Jordan Retro 4 White Cement",
+    precio: "111.000",
+  },
+  {
+    imagen: "../img/jordan 4/Air Jordan 4 Retro 'Mushroom'.jpeg",
+    id: 64,
+    nombre: "Air Jordan Retro 4 Mushroom",
+    precio: "103.000",
+  },
+
+  //AIR JORDAN 5//
+
+  {
+    imagen: "../img/jordan 5/Air Jordan 5 Retro 'Blue Bird'.jpeg",
+    id: 65,
+    nombre: "Air Jordan Retro 5 Blue Bird",
+    precio: "185.000",
+  },
+  {
+    imagen: "../img/jordan 5/Air Jordan 5 Retro 'International Flight'.jpeg",
+    id: 66,
+    nombre: "Air Jordan Retro 5 International Flight",
+    precio: "154.000",
+  },
+  {
+    imagen: "../img/jordan 5/Air Jordan 5 Retro 'Jade Horizon'.jpeg",
+    id: 67,
+    nombre: "Air Jordan Retro 5 Jade Horizon",
+    precio: "150.000",
+  },
+  {
+    imagen: "../img/jordan 5/Air Jordan 5 Retro 'Oil Grey'.jpeg",
+    id: 68,
+    nombre: "Air Jordan Retro 5 Oil Grey",
+    precio: "143.000",
+  },
+  {
+    imagen: "../img/jordan 5/Air Jordan 5 Retro 'Racer Blue'.jpeg",
+    id: 69,
+    nombre: "Air Jordan Retro 5 Racer Blue",
+    precio: "170.000",
+  },
+  {
+    imagen: "../img/jordan 5/Air Jordan 5 Retro 'Shattered Backboard'.jpg",
+    id: 70,
+    nombre: "Air Jordan Retro 5 Shattered Backboard",
+    precio: "160.000",
+  },
+  {
+    imagen: "../img/jordan 5/Air Jordan 5 Retro SE 'UNC'.webp",
+    id: 71,
+    nombre: "Air Jordan Retro 5 UNC",
+    precio: "200.000",
+  },
+  {
+    imagen: "../img/jordan 5/Air Jordan 5 X OFF-WHITE.jpg",
+    id: 72,
+    nombre: "Air Jordan Retro 5 x OFF-WHITE",
+    precio: "300.000",
+  },
+  {
+    imagen: "../img/jordan 5/Clot x Air Jordan 5 Low.webp",
+    id: 73,
+    nombre: "Air Jordan Retro 5 low x Clot",
+    precio: "186.000",
+  },
+  {
+    imagen: "../img/jordan 5/Jordan 5 Retro Island Green.jpeg",
+    id: 74,
+    nombre: "Air Jordan Retro 5 Island Green",
+    precio: "145.000",
+  },
+  {
+    imagen: "../img/jordan 5/Jordan 5 Retro Low Alternate 90.jpeg",
+    id: 75,
+    nombre: "Air Jordan Retro 5 low Alternate 90",
+    precio: "220.000",
+  },
+  {
+    imagen: "../img/jordan 5/Jordan 5 Retro Low Wolf Grey (GS).jpeg",
+    id: 76,
+    nombre: "Air Jordan Retro 5 low Wolf Grey",
+    precio: "157.000",
+  },
+];
